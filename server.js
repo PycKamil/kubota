ed49d1488103a2073509f04e5c158f4b794a0cdf
Kamil Pyc <kamil.pyc@allegrogroup.com>
2016-04-15 23:13:54 +0200

    no message

--- server.js
+++ server.js
@@ -11,8 +11,7 @@ var pageToken = "EAAOA7gj8NdYBAKehCNkZCoJJqDLD7Jzi3c9IIDANxEFjZAlqUZAtGTbwPS0t2k
 
 var port = process.env.PORT || 1337;
 
-var dialog = new builder.LuisDialog('https://api.projectoxford.ai/luis/v1/application?id=e252a847-190c-4341-b4d2-105acc75f898&subscri...')
-var bot = new builder.TextBot();
+var dialog = new builder.LuisDialog('https://api.projectoxford.ai/luis/v1/application?id=e252a847-190c-4341-b4d2-105acc75f898&subscription-key=5349414a86334241b0bfe3254648fa52')var bot = new builder.TextBot();
 // bot.add('/', function (session) {
 //
 //     //respond with user's message
