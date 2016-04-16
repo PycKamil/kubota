f7c71671bdb1f65f1d87893f28fe2e230ccb64d3
Remigiusz Herba <remigiusz.herba@allegrogroup.com>
2016-04-16 05:37:50 +0200

    awe

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ var popularItems = [];
 dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
 dialog.on('showMe', [
     function (session, args, next) {
-    message = new builder.Message()
-    message.SIATECZKAIMAGE = "https://upload.wikimedia.org/wikipedia/commons/4/4f/Dziewczynka_z_wazonem_z_kwiatami,1902.jpg"
+      message = new builder.Message()
+      message.SIATECZKAIMAGE = "https://upload.wikimedia.org/wikipedia/commons/4/4f/Dziewczynka_z_wazonem_z_kwiatami,1902.jpg"
       session.send(message)
       session.send("Obraz „Helenka z wazonem” został namalowany przez Stanisława Wyspiańskiego w 1902 roku. Obecnie można go podziwiać w Muzeum Narodowym w Krakowie.")
     },
@@ -46,6 +46,7 @@ app.get('/webhook/', function (req, res) {
           bot.add('/', dialog);
           bot.on('reply', function (message) {
             if (message.SIATECZKAIMAGE) {
+              sendImageMessage(sender, "a to chociaz dziala?")
               sendImageMessage(sender, message.SIATECZKAIMAGE)
             } else if (message.elements) {
               sendGenericMessage(sender, message.elements)
