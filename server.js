c0858d8b8f03d02a61ee6612d1b49a4acc38a368
Kamil Pyc <kamil.pyc@allegrogroup.com>
2016-04-16 08:22:59 +0200

    new error message

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var siateczka = false
 var dialog = new builder.LuisDialog('https://api.projectoxford.ai/luis/v1/application?id=e252a847-190c-4341-b4d2-105acc75f898&subscription-key=5349414a86334241b0bfe3254648fa52');
 var popularItems = [];
 
-dialog.onDefault(builder.DialogAction.send("I'm sorry. I didn't understand."));
+dialog.onDefault(builder.DialogAction.send("Hm, nie do końca rozumiem… Możesz sformułować to nieco prościej?"));
 dialog.on('showMe', [
     function (session, args, next) {
       message = new builder.Message()
